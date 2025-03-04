c391445a47f164ede94c3447f755096041d9ffc1
seungwon <swlee173@gmail.com>
2025-03-04 17:33:13 +0900

    Initial commit

---
+++ src/test/java/com/cs/calendarback/CalendarBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.calendarback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
