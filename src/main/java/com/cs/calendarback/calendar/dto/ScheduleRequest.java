8bc7d0b783190defc820320a9d371e1ede3d623f
seungwon <swlee173@gmail.com>
2025-03-06 23:30:25 +0900

    feat: 일정 API 문서화 및 유효성 검증 추가
    - 일정 API에서 @Valid 어노테이션을 사용하여 요청 유효성 검증
    - 일정 API에 대한 Swagger 문서 추가
    - ScheduleRequest DTO에 @NotBlank 유효성 검증 추가
    - ErrorResponse DTO에 @Schema 어노테이션을 통해 Swagger 문서화 추가

--- src/main/java/com/cs/calendarback/calendar/dto/ScheduleRequest.java
+++ src/main/java/com/cs/calendarback/calendar/dto/ScheduleRequest.java
@@ -1,17 +1,25 @@
 package com.cs.calendarback.calendar.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 
 public record ScheduleRequest(
+        @NotBlank(message = "제목은 필수 입력 값입니다.")
         String title,
+
         String note,
+
+        @NotBlank(message = "시작 일정은 필수 입력 값입니다.")
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime startDateTime,
 
+        @NotBlank(message = "종료 일정은 필수 입력 값입니다.")
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime endDateTime,
+
+        @NotBlank(message = "사용자 ID 필수 입력 값입니다.")
         Long userId
 ) {
 }
