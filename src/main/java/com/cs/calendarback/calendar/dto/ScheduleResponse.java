b1c44b533b5e2f58256693c6072c953cf4612b2d
seungwon <swlee173@gmail.com>
2025-03-14 19:19:52 +0900

    refactor: 시작/종료 날짜를 LocalDateTime에서 String으로 변경

--- src/main/java/com/cs/calendarback/calendar/dto/ScheduleResponse.java
+++ src/main/java/com/cs/calendarback/calendar/dto/ScheduleResponse.java
@@ -1,31 +1,37 @@
 package com.cs.calendarback.calendar.dto;
 
 import com.cs.calendarback.calendar.entity.Schedule;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public record ScheduleResponse(
         Long id,
         String title,
         String note,
-        LocalDateTime startDateTime,
-        LocalDateTime endDateTime,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        String startDateTime,
+
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        String endDateTime,
         Long userId
 ) {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     public static ScheduleResponse from(Schedule schedule) {
         return new ScheduleResponse(
                 schedule.getId(),
                 schedule.getTitle(),
                 schedule.getNote(),
-                schedule.getStartDateTime(),
-                schedule.getEndDateTime(),
+                schedule.getStartDateTime().format(FORMATTER),
+                schedule.getEndDateTime().format(FORMATTER),
                 schedule.getUserId()
         );
     }
 
-    public static List<ScheduleResponse> from(List<Schedule> Schedules) {
-        return Schedules.stream()
+    public static List<ScheduleResponse> from(List<Schedule> schedules) {
+        return schedules.stream()
                 .map(ScheduleResponse::from)
                 .toList();
     }
--- src/main/java/com/cs/calendarback/config/exception/ErrorType.java
+++ src/main/java/com/cs/calendarback/config/exception/ErrorType.java
@@ -8,7 +8,8 @@ import org.springframework.boot.logging.LogLevel;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorType {
-    SCHEDULE_NOT_FOUND(ErrorCode.NOT_FOUND, "일정을 찾을 수 없습니다.", LogLevel.WARN);
+    SCHEDULE_NOT_FOUND(ErrorCode.NOT_FOUND, "일정을 찾을 수 없습니다.", LogLevel.WARN),
+    INVALID_DATE_FORMAT(ErrorCode.BAD_REQUEST, "날짜의 형식이 올바르지 않습니다.", LogLevel.WARN);
 
     private final ErrorCode errorCode;
     private final String message;
