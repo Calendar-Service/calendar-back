68b146b708df417d2445a4a45f0752877cb26f7d
seungwon <swlee173@gmail.com>
2025-03-20 23:26:56 +0900

    remove: JWT 시큐리티 제거

--- src/main/java/com/cs/calendarback/calendar/dto/MemberRequest.java
+++ src/main/java/com/cs/calendarback/calendar/dto/MemberRequest.java
@@ -1,6 +1,5 @@
 package com.cs.calendarback.calendar.dto;
 
-import com.cs.calendarback.calendar.entity.enums.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
@@ -13,8 +12,6 @@ public record MemberRequest(
         String email,
 
         @NotBlank(message = "패스워드는 필수 입력 값입니다.")
-        String password,
-
-        Role role
+        String password
 ) {
 }
--- src/main/java/com/cs/calendarback/calendar/entity/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.cs.calendarback.calendar.entity.enums;
-
-public enum Role {
-    ROLE_ADMIN, ROLE_USER
-}
