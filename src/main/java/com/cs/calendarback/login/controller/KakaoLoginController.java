a6b3f0da2b78a93c87bc4b5b9bb2b8877d09e286
seungwonlee <swlee173@gmail.com>
2025-07-14 13:54:58 +0900

    feat: 로키 설정 및 테스트

--- src/main/java/com/cs/calendarback/login/controller/KakaoLoginController.java
+++ src/main/java/com/cs/calendarback/login/controller/KakaoLoginController.java
@@ -1,58 +1,58 @@
-package com.cs.calendarback.login.controller;
-
-import com.cs.calendarback.login.dto.KakaoLoginResponse;
-import com.cs.calendarback.login.dto.KakaoTokenResponse;
-import com.cs.calendarback.member.service.MemberService;
-import com.cs.calendarback.login.dto.KakaoMemberResponse;
-import com.cs.calendarback.login.service.KakaoService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/api/v1/logins")
-public class KakaoLoginController {
-    private final KakaoService kakaoService;
-    private final MemberService memberService;
-
-    @GetMapping("/oauth/kakao/callback")
-    public ResponseEntity<?> callback(@RequestParam("code") String code, HttpServletResponse response) {
-
-        KakaoTokenResponse kakaoToken = kakaoService.getAccessTokenFromKakao(code);
-        KakaoMemberResponse kakaoMember = kakaoService.getMemberInfo(kakaoToken.getAccessToken());
-        Long memberId = memberService.createKakaoMember(kakaoMember);
-        KakaoLoginResponse kakaoLogin = KakaoLoginResponse.of(memberId);
-
-        response.setHeader("Authorization", "Bearer " + kakaoToken.getAccessToken());
-        response.addCookie(createCookie("refresh", kakaoToken.getRefreshToken()));
-
-        return ResponseEntity.ok(kakaoLogin);
-    }
-
-
-    @GetMapping("/oauth/kakao/logout")
-    public ResponseEntity<?> kakaoLogout(@RequestHeader("Authorization") String accessToken, HttpServletResponse response) {
-        kakaoService.kakaoLogout(accessToken);
-        response.addCookie(deleteCookie("refresh"));
-        return ResponseEntity.ok("카카오 로그아웃 완료");
-    }
-
-    private Cookie createCookie(String key, String value) {
-        Cookie cookie = new Cookie(key, value);
-        cookie.setMaxAge(24 * 60 * 60);
-        // cookie.setSecure(true);
-        // cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        return cookie;
-    }
-
-    private Cookie deleteCookie(String key) {
-        Cookie cookie = new Cookie(key, null);
-        cookie.setMaxAge(0);  // 쿠키 즉시 삭제
-        cookie.setPath("/");   // 모든 경로에서 삭제 적용
-        return cookie;
-    }
-}
+//package com.cs.calendarback.login.controller;
+//
+//import com.cs.calendarback.login.dto.KakaoLoginResponse;
+//import com.cs.calendarback.login.dto.KakaoTokenResponse;
+//import com.cs.calendarback.member.service.MemberService;
+//import com.cs.calendarback.login.dto.KakaoMemberResponse;
+//import com.cs.calendarback.login.service.KakaoService;
+//import jakarta.servlet.http.Cookie;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/api/v1/logins")
+//public class KakaoLoginController {
+//    private final KakaoService kakaoService;
+//    private final MemberService memberService;
+//
+//    @GetMapping("/oauth/kakao/callback")
+//    public ResponseEntity<?> callback(@RequestParam("code") String code, HttpServletResponse response) {
+//
+//        KakaoTokenResponse kakaoToken = kakaoService.getAccessTokenFromKakao(code);
+//        KakaoMemberResponse kakaoMember = kakaoService.getMemberInfo(kakaoToken.getAccessToken());
+//        Long memberId = memberService.createKakaoMember(kakaoMember);
+//        KakaoLoginResponse kakaoLogin = KakaoLoginResponse.of(memberId);
+//
+//        response.setHeader("Authorization", "Bearer " + kakaoToken.getAccessToken());
+//        response.addCookie(createCookie("refresh", kakaoToken.getRefreshToken()));
+//
+//        return ResponseEntity.ok(kakaoLogin);
+//    }
+//
+//
+//    @GetMapping("/oauth/kakao/logout")
+//    public ResponseEntity<?> kakaoLogout(@RequestHeader("Authorization") String accessToken, HttpServletResponse response) {
+//        kakaoService.kakaoLogout(accessToken);
+//        response.addCookie(deleteCookie("refresh"));
+//        return ResponseEntity.ok("카카오 로그아웃 완료");
+//    }
+//
+//    private Cookie createCookie(String key, String value) {
+//        Cookie cookie = new Cookie(key, value);
+//        cookie.setMaxAge(24 * 60 * 60);
+//        // cookie.setSecure(true);
+//        // cookie.setPath("/");
+//        cookie.setHttpOnly(true);
+//        return cookie;
+//    }
+//
+//    private Cookie deleteCookie(String key) {
+//        Cookie cookie = new Cookie(key, null);
+//        cookie.setMaxAge(0);  // 쿠키 즉시 삭제
+//        cookie.setPath("/");   // 모든 경로에서 삭제 적용
+//        return cookie;
+//    }
+//}
--- src/main/java/com/cs/calendarback/login/service/KakaoService.java
+++ src/main/java/com/cs/calendarback/login/service/KakaoService.java
@@ -1,82 +1,82 @@
-package com.cs.calendarback.login.service;
-
-import com.cs.calendarback.config.exception.CoreException;
-import com.cs.calendarback.config.exception.ErrorType;
-import com.cs.calendarback.login.dto.KakaoTokenResponse;
-import com.cs.calendarback.login.dto.KakaoMemberResponse;
-import io.netty.handler.codec.http.HttpHeaderValues;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-
-@Slf4j
-@RequiredArgsConstructor
-@Service
-public class KakaoService {
-
-    @Value("${kakao.client_id}")
-    private String clientId;
-
-    private static final String KAKAO_AUTH_TOKEN_URL_HOST = "https://kauth.kakao.com";
-    private static final String KAKAO_AUTH_USER_URL_HOST = "https://kapi.kakao.com";
-    private static final String KAKAO_LOGOUT_URL = "https://kapi.kakao.com/v1/user/logout";
-
-
-    public KakaoTokenResponse getAccessTokenFromKakao(String code) {
-
-        KakaoTokenResponse kakaoTokenResponseDto = WebClient.create(KAKAO_AUTH_TOKEN_URL_HOST).post()
-                .uri(uriBuilder -> uriBuilder.scheme("https").path("/oauth/token")
-                        .queryParam("grant_type", "authorization_code")
-                        .queryParam("client_id", clientId)
-                        .queryParam("code", code)
-                        .build(true))
-                .header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INVALID_PARAMETER, "getAccessTokenForKakao")))
-                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INTERNAL_SERVER_ERROR, "getAccessTokenForKakao")))
-                .bodyToMono(KakaoTokenResponse.class)
-                .block();
-
-        log.info(" [Kakao Service] Access Token ------> {}", kakaoTokenResponseDto.getAccessToken());
-        log.info(" [Kakao Service] Refresh Token ------> {}", kakaoTokenResponseDto.getRefreshToken());
-
-        return kakaoTokenResponseDto;
-    }
-
-    public KakaoMemberResponse getMemberInfo(String accessToken) {
-
-        KakaoMemberResponse userInfo = WebClient.create(KAKAO_AUTH_USER_URL_HOST).get()
-                .uri(uriBuilder -> uriBuilder.scheme("https").path("/v2/user/me").build(true))
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
-                .header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INVALID_PARAMETER,  "memberInfo")))
-                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INTERNAL_SERVER_ERROR, "memberInfo")))
-                .bodyToMono(KakaoMemberResponse.class)
-                .block();
-
-        log.info("[ Kakao Service ] Auth ID ---> {} ", userInfo.getId());
-        log.info("[ Kakao Service ] NickName ---> {} ", userInfo.getKakaoAccount().getProfile().getNickName());
-
-        return userInfo;
-    }
-
-
-    public void kakaoLogout(String accessToken) {
-        WebClient.create()
-                .post()
-                .uri(KAKAO_LOGOUT_URL)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INVALID_PARAMETER, "logout")))
-                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INTERNAL_SERVER_ERROR, "logout")))
-                .bodyToMono(String.class)
-                .block();
-    }
-}
+//package com.cs.calendarback.login.service;
+//
+//import com.cs.calendarback.config.exception.CoreException;
+//import com.cs.calendarback.config.exception.ErrorType;
+//import com.cs.calendarback.login.dto.KakaoTokenResponse;
+//import com.cs.calendarback.login.dto.KakaoMemberResponse;
+//import io.netty.handler.codec.http.HttpHeaderValues;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatusCode;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.reactive.function.client.WebClient;
+//import reactor.core.publisher.Mono;
+//
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//@Service
+//public class KakaoService {
+//
+//    @Value("${kakao.client_id}")
+//    private String clientId;
+//
+//    private static final String KAKAO_AUTH_TOKEN_URL_HOST = "https://kauth.kakao.com";
+//    private static final String KAKAO_AUTH_USER_URL_HOST = "https://kapi.kakao.com";
+//    private static final String KAKAO_LOGOUT_URL = "https://kapi.kakao.com/v1/user/logout";
+//
+//
+//    public KakaoTokenResponse getAccessTokenFromKakao(String code) {
+//
+//        KakaoTokenResponse kakaoTokenResponseDto = WebClient.create(KAKAO_AUTH_TOKEN_URL_HOST).post()
+//                .uri(uriBuilder -> uriBuilder.scheme("https").path("/oauth/token")
+//                        .queryParam("grant_type", "authorization_code")
+//                        .queryParam("client_id", clientId)
+//                        .queryParam("code", code)
+//                        .build(true))
+//                .header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
+//                .retrieve()
+//                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INVALID_PARAMETER, "getAccessTokenForKakao")))
+//                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INTERNAL_SERVER_ERROR, "getAccessTokenForKakao")))
+//                .bodyToMono(KakaoTokenResponse.class)
+//                .block();
+//
+//        log.info(" [Kakao Service] Access Token ------> {}", kakaoTokenResponseDto.getAccessToken());
+//        log.info(" [Kakao Service] Refresh Token ------> {}", kakaoTokenResponseDto.getRefreshToken());
+//
+//        return kakaoTokenResponseDto;
+//    }
+//
+//    public KakaoMemberResponse getMemberInfo(String accessToken) {
+//
+//        KakaoMemberResponse userInfo = WebClient.create(KAKAO_AUTH_USER_URL_HOST).get()
+//                .uri(uriBuilder -> uriBuilder.scheme("https").path("/v2/user/me").build(true))
+//                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+//                .header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
+//                .retrieve()
+//                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INVALID_PARAMETER,  "memberInfo")))
+//                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INTERNAL_SERVER_ERROR, "memberInfo")))
+//                .bodyToMono(KakaoMemberResponse.class)
+//                .block();
+//
+//        log.info("[ Kakao Service ] Auth ID ---> {} ", userInfo.getId());
+//        log.info("[ Kakao Service ] NickName ---> {} ", userInfo.getKakaoAccount().getProfile().getNickName());
+//
+//        return userInfo;
+//    }
+//
+//
+//    public void kakaoLogout(String accessToken) {
+//        WebClient.create()
+//                .post()
+//                .uri(KAKAO_LOGOUT_URL)
+//                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+//                .retrieve()
+//                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INVALID_PARAMETER, "logout")))
+//                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new CoreException(ErrorType.KAKAO_INTERNAL_SERVER_ERROR, "logout")))
+//                .bodyToMono(String.class)
+//                .block();
+//    }
+//}
